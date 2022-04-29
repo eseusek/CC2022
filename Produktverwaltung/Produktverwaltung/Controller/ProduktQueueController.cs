61a323a50c0c39c6be8a356e8d71c38078bd8ef5
eseusek <33778537+eseusek@users.noreply.github.com>
2022-04-29 18:13:16 +0200

    ProduktQueueController hinzugefügt
    ProduktQueueController hinzugefügt

---
+++ Produktverwaltung/Produktverwaltung/Controller/ProduktQueueController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Produktverwaltung.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProduktQueueController : ControllerBase
+    {
+        // GET: api/<ProduktQueueController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProduktQueueController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProduktQueueController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProduktQueueController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProduktQueueController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
